626df072cbb2e5f1cf3f98465bb7c4b71c937359
Ankur Semle <semleankur@gmail.com>
2024-02-15 14:27:15 +0530

    add data in constants

---
+++ src/Constants.js
@@ -0,0 +1,7 @@
+export const data = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmXxKQDqIBdARzhNjly0V2ePKI1VW-aUS1Eg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjC8995lmnEvCsevvxEcWz0ZKdE2RlDnj1gQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhycx9nVCTSaLxxl2NJcmK0gyZAHpqw3kM1A&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTft7FrLGWFOb4-1Yz5WPfoIuBofRcIjXyglw&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIe_z5uELfX9jakvPlYa5oCZJ0odTedIW1Qg&usqp=CAU",
+  ];
