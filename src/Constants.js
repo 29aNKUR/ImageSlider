46f45e87d4a09f37015d1682232481ad562dbfa4
Ankur Semle <semleankur@gmail.com>
2024-02-15 17:21:48 +0530

    imageSlider with objects instead of img urls

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,31 @@
+// export const data = [
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmXxKQDqIBdARzhNjly0V2ePKI1VW-aUS1Eg&usqp=CAU",
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjC8995lmnEvCsevvxEcWz0ZKdE2RlDnj1gQ&usqp=CAU",
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhycx9nVCTSaLxxl2NJcmK0gyZAHpqw3kM1A&usqp=CAU",
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTft7FrLGWFOb4-1Yz5WPfoIuBofRcIjXyglw&usqp=CAU",
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIe_z5uELfX9jakvPlYa5oCZJ0odTedIW1Qg&usqp=CAU",
+//   ];
+
 export const data = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmXxKQDqIBdARzhNjly0V2ePKI1VW-aUS1Eg&usqp=CAU",
+  {
+    id: 1,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmXxKQDqIBdARzhNjly0V2ePKI1VW-aUS1Eg&usqp=CAU",
+    title: "Item 1",
+    description: "Description of item 1",
+  },
+  {
+    id: 2,
+    imageUrl:
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjC8995lmnEvCsevvxEcWz0ZKdE2RlDnj1gQ&usqp=CAU",
+    title: "Item 2",
+    description: "Description of item 2",
+  },
+  {
+    id: 3,
+    imageUrl:
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhycx9nVCTSaLxxl2NJcmK0gyZAHpqw3kM1A&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTft7FrLGWFOb4-1Yz5WPfoIuBofRcIjXyglw&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIe_z5uELfX9jakvPlYa5oCZJ0odTedIW1Qg&usqp=CAU",
-  ];
+    title: "Item 3",
+    description: "Description of item 3",
+  },
+];
--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -10,18 +10,30 @@ const ImageSlider = () => {
   const handleNext = () => {
     setActiveIndex(activeIndex === data.length - 1 ? 0 : activeIndex + 1);
   };
- 
-    return (
-        <div >
-          <div className="flex justify-center mt-24">
-            <button className="font-bold p-10 m-10" onClick={handlePrev}>Previous</button>
-            {data.map((url,i) => <img key={url} src={url} alt="slider" className={`w-[700px] h-[500px] ${(activeIndex === i ? "block" : "hidden")}`}/>)}
-          <button className="font-bold p-10 m-10" onClick={handleNext}>Next</button>
+
+  console.log(data.map((item) => item));
+  return (
+    <div className="flex justify-center">
+      <div>
+        <button className="font-bold p-10 m-10" onClick={handlePrev}>
+          Previous
+        </button>
+        {data.map((item, i) => (
+          <div className="flex flex-col justify-center">
+            <img src={item.imageUrl} alt="slider" className={`w-[200px] h-[200px] ${(activeIndex === i ? "block" : "hidden")}`}/>
+            <h1 className={`${(activeIndex === i ? "block" : "hidden")}`}>{item.title}</h1>
+            <h1 className={`${(activeIndex === i ? "block" : "hidden")}`}>{item.description}</h1>
           </div>
-   
-        </div>
-    )
- 
-}
+        ))}
+
+        {/* {data.map((url,i) => <img key={url} src={url} alt="slider" className={`w-[700px] h-[500px] ${(activeIndex === i ? "block" : "hidden")}`}/>)} */}
+
+        <button className="font-bold p-10 m-10" onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ImageSlider;
